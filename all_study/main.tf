cc2afea9ab4ace9377404dd84f4d0b193cfd128d
maruchan76 <city.mati76@gmail.com>
2019-09-26 21:54:08 +0900

    書き方のBASE

--- all_study/main.tf
+++ all_study/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "ap-northeast-1"
 }
 
-data "aws_iam_policy_document" "allow_describe_regions" {
+data "aws_iam_policy_document" "allow_describe_regions_data" {
   statement {
     effect      = "Allow"
     actions     = ["ec2:DescribeRegions"]
@@ -10,12 +10,12 @@ data "aws_iam_policy_document" "allow_describe_regions" {
   }
 }
 
-resource "aws_iam_policy" "exa" {
-  name    = "iam_oreo"
-  policy  = data.aws_iam_policy_document.allow_describe_regions.json
+resource "aws_iam_policy" "iam_policy_resource" {
+  name    = "iam_ec2_name"
+  policy  = data.aws_iam_policy_document.allow_describe_regions_data.json
 }
 
-data "aws_iam_policy_document" "ec2_assume_role" {
+data "aws_iam_policy_document" "ec2_assume_role_data" {
   statement {
     actions = ["sts:AssumeRole"]
 
