a09e2c5c1736016c01213a751cf84875af2fd08b
maruchan76 <city.mati76@gmail.com>
2019-09-30 23:56:16 +0900

    勘違いをしていたので修正

--- all_study/main.tf
+++ all_study/main.tf
@@ -2,5 +2,9 @@ module "describe_regions_for_ec2" {
   source     = "./iam_role"
   name       = "describe-regions-for-ec2"
   identifier = "ec2.amazonaws.com"
-  policy     = data.aws_iam_policy_document.sample.json
+  policy     = data.aws_iam_policy_document.allow_describe_regions.json
 }
+
+# provider "aws" {
+#     region = "ap-northeast-1"
+# }
