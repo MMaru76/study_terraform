359d95679e31c8caca60dd1a3d7b5a3022c5317a
maruchan76 <city.mati76@gmail.com>
2019-12-27 21:08:12 +0900

    IAMの完全設定

--- hello/iam_role/main.tf
+++ hello/iam_role/main.tf
@@ -1,13 +1,13 @@
-variable "name" {}
-variable "policy" {}
+# variable "name" {}
+# variable "policy" {}
 variable "identifier" {}
 
-resource "aws_iam_role" "default" {
-  name               = var.name
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+resource "aws_iam_role" "tabiya01" {
+  name               = "tabiya-role"
+  assume_role_policy = data.aws_iam_policy_document.tabiya_assume.json
 }
 
-data "aws_iam_policy_document" "assume_role" {
+data "aws_iam_policy_document" "tabiya_assume" {
   statement {
     actions = ["sts:AssumeRole"]
 
@@ -18,20 +18,28 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-resource "aws_iam_policy" "default" {
-  name   = var.name
-  policy = var.policy
+resource "aws_iam_policy" "tabiya02" {
+  name   = "tabiya-policy"
+  policy = data.aws_iam_policy_document.allow_describe_regions.json
 }
 
-resource "aws_iam_role_policy_attachment" "default" {
-  role       = aws_iam_role.default.name
-  policy_arn = aws_iam_policy.default.arn
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:DescribeRegions"] # リージョン一覧を取得する
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "tabiya03" {
+  role       = aws_iam_role.tabiya01.name
+  policy_arn = aws_iam_policy.tabiya02.arn
 }
 
 output "iam_role_arn" {
-  value = aws_iam_role.default.arn
+  value = aws_iam_role.tabiya01.arn
 }
 
 output "iam_role_name" {
-  value = aws_iam_role.default.name
+  value = aws_iam_role.tabiya01.name
 }
